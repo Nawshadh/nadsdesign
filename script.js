142a6074148414db87ec2f37e38bab62bc4fc9d2
N4DDY <85053224+N4DDY@users.noreply.github.com>
2021-06-19 04:21:30 +0530

    small update

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 $(document).ready(function(){
     $(window).scroll(function(){
+        // sticky navbar on scroll script
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
 
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
@@ -15,6 +34,22 @@ $(document).ready(function(){
         
     });
    
+    // typing animation scrit
+    var typed = new Typed(".typing", {
+        strings: ["video editor", "Graphic designer", "Web designer", "Motion Graphics Designer","Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing2", {
+        strings: ["video editor", "Graphic designer", "Web designer", "Motion Graphics Designer","Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+
     // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
